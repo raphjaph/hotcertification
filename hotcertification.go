5ad9afa15f74f1c13548d3eba72124449ed03388
Raphael <raphael@sophon.fritz.box>
2021-05-05 18:11:08 +0200

    merged options and hotcertification.go

--- hotcertification.go
+++ hotcertification.go
@@ -12,6 +12,38 @@ import (
 	"github.com/raphasch/hotcertification/protocol"
 )
 
+// Information other replicas in network have to know about each other (public knowledge)
+// Private knowledge (threshold key, ecdsa private key) have to given through command line and not in global config file
+type Peer struct {
+	ID                  hotstuff.ID
+	PubKey              string `mapstructure:"pubkey"`
+	TLSCert             string `mapstructure:"tls-cert"`
+	ClientAddr          string `mapstructure:"client-address"`
+	ReplicationPeerAddr string `mapstructure:"replication-peer-address"`
+	SigningPeerAddr     string `mapstructure:"signing-peer-address"`
+}
+
+type Options struct {
+	// The ID of this server
+	ID hotstuff.ID `mapstructure:"id"`
+
+	// TLS configs
+	RootCA  string `mapstructure:"root-ca"`
+	TLS     bool   `mapstructure:"tls"`
+	PrivKey string `mapstructure:"privkey"` // privkey has to belong the to the pubkey and should be ecdsa because thresholdkey can't do TLS
+
+	// HotStuff configs
+	PmType      string      `mapstructure:"pacemaker"`
+	LeaderID    hotstuff.ID `mapstructure:"leader-id"`
+	ViewTimeout int         `mapstructure:"view-timeout"`
+
+	// HotCertification and miscellaneous configs
+	ThresholdKey string `mapstructure:"thresholdkey"`
+	KeySize      int    `mapstructure:"key-size"`
+	ConfigFile   string `mapstructure:"config"`
+	Peers        []Peer
+}
+
 type RequestInfo struct {
 	CSR         *protocol.CSR
 	Certificate *x509.Certificate
