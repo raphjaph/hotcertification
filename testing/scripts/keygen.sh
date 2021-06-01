9002943d52cf0d3a4e378492db07dffc0d6473ea
root <root@bitcoingold.blockchain.net.in.tum.de>
2021-06-02 00:31:20 +0200

    script stuff

---
+++ testing/scripts/keygen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script starts a docker container to generate the keys (trusted setup phase)
+# The keys are then stored on host machine in the specified folder
+# This script should also generate a config file 
+# Then config file and keys are organized into node folders
+
+
+docker run  --name "keygen" -v "/root/hotcertification/testing:/home" raphasch/hotcertification keygen -n 4 -t 3 --key-size 512 keys
+docker container prune
