e389e770d184c7b9baf57b84a9cdaaf3a8119b97
Raphael Schleithoff <schleith@coinbase>
2021-06-01 23:44:32 +0200

    testbed setup script done

--- testing/setup.sh
+++ testing/setup.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-#pos allocations allocate bitcoingold
+echo "Allocating bitcoingold"
+pos allocations allocate bitcoingold
 
-#pos nodes image bitcoingold debian-buster
+pos nodes image bitcoingold debian-buster
 
-#pos nodes reset bitcoingold
+echo "Restarting machine with new OS"
+pos nodes reset bitcoingold
 
-scp -r /home/schleith/hotcertification bitcoingold:~
+echo "Copying repo to machine"
+scp -q -r /home/schleith/hotcertification bitcoingold:~
 
+echo "Installing docker on machine"
 pos commands launch bitcoingold -- sh hotcertification/testing/install-docker-debian.sh
 
+echo "Pulling hotcertification image from docker hub"
+pos commands launch bitcoingold -- docker pull raphasch/hotcertification
 
