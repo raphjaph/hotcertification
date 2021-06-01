eba472e293faa9a578f0b9d8ddf4cf5576c9d144
Raphael <raphael.schleithoff@tum.de>
2021-06-01 16:46:05 +0200

    docker compose test setup works

--- run_servers_locally.sh
+++ run_servers_locally.sh
@@ -6,10 +6,10 @@ trap 'trap - SIGTERM && kill -- -$$' SIGINT SIGTERM EXIT
 
 bin='cmd/certificationserver/certserver'
 
-$bin --id 1 --thresholdkey keys/p1.thresholdkey --privkey keys/p1.key &
-$bin --id 2 --thresholdkey keys/p2.thresholdkey --privkey keys/p2.key 2> logs/2.out &
-$bin --id 3 --thresholdkey keys/p3.thresholdkey --privkey keys/p3.key 2> logs/3.out &
-$bin --id 4 --thresholdkey keys/p4.thresholdkey --privkey keys/p4.key 2> logs/4.out &
+$bin --id 1 --thresholdkey keys/n1.thresholdkey --privkey keys/n1.key &
+$bin --id 2 --thresholdkey keys/n2.thresholdkey --privkey keys/n2.key 2> logs/2.out &
+$bin --id 3 --thresholdkey keys/n3.thresholdkey --privkey keys/n3.key 2> logs/3.out &
+$bin --id 4 --thresholdkey keys/n4.thresholdkey --privkey keys/n4.key 2> logs/4.out &
 
 if [ "$1" = "kill" ]; then
 	sleep 5s
